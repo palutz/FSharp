51f82f8c280b79c681830f89effbd5db30d43fce
palutz <stefano.paluello@gmail.com>
2012-11-24 12:09:08 +0100

    three

--- 01_stockData.fs
+++ 01_stockData.fs
@@ -34,3 +34,92 @@ stockData
 |> List.map splitCommas
 |> List.maxBy (fun x -> abs(float x.[1] - float x.[4]))
 |> (fun x -> x.[0])
+
+(*
+splitCommas takes a string and breaks it into pieces whenever it finds a comma. 
+String.Split is a function that splits a string based on takes an array of separator characters. Arrays are another basic container type in F# that allow you to index stored elements.
+You create arrays using a [| ... |] which is similar to the [ .. ] syntax used for lists. Here,[|','|] creates a single element array containing a comma. The result of the splitCommas is also an array. This array contains the individual string parts.
+For example, parsing the last line of the input yields the array [| 2012-02-29; 31.89; 32.00; 31.61; 31.74; 59323600; 31.74 |].
+
+Now that you've separated the data by commas, you need to find the maximum variance between the opening and closing days. You accomplish that using the List.maxBy function, which finds the maximum item in a List based a given comparison function. You use a comparison function of (fun x -> abs(float x.[1] - float x.[4])) to find the maximum variance. Note that abs is a function for calculating absolute value, and float is a function to parse a floating point number from a string. The x.[1] and x.[4] calls get the second and fifth elements in the array, respectively (in F# array indexing is zero-based).
+
+Finally, you project the date from the maximum row using List.map and a projection function of (fun x -> x.[0]).
+*)
+
+// Record type
+type Book = 
+  { Name: string;
+    AuthorName: string;
+    Rating: int;
+    ISBN: string }
+
+let expertFSharp = 
+  { Name = "Expert F#";
+    AuthorName = "Don Syme, Adam Granicz, Antonio Cisternino";
+    Rating = 5;
+    ISBN = "1590598504" }
+
+ // using let expertFSharp is immutable... to change we can clone another record with the same data and change only what we need
+let partDeux = { expertFSharp with Name = "Expert F# 2.0" }
+
+// USING OPTION (optional parameter)
+type Book =
+  { Name: string;
+    AuthorName: string;
+    Rating: int option;
+    ISBN: string }
+// ERROR!!! Rating missing....
+let unratedEdition = 
+  { Name = "Expert F#";
+    AuthorName = "Don Syme, Adam Granicz, Antonio Cisternino";
+    ISBN = "1590598504" }
+// Correct with empty/none value
+let unratedEdition = 
+  { Name = "Expert F#";
+    AuthorName = "Don Syme, Adam Granicz, Antonio Cisternino";
+    Rating = None;    
+    ISBN = "1590598504" }
+// giving a value to option
+let stingyReview = 
+  { Name = "Expert F#";
+    AuthorName = "Don Syme, Adam Granicz, Antonio Cisternino";
+    Rating = Some 1;    // without Some is wrong!! 
+    ISBN = "1590598504" }
+
+// Using MATCH (looking for record where an option field exists)
+let printRating book =
+    match book.Rating with
+    | Some rating -> printfn "I give this book %d star(s) out of 5!" rating
+    | None -> printfn "I didn't review this book"
+
+// DISCRIMIATED UNIONS
+type PowerUp =
+| FireFlower
+| Mushroom
+| Star
+
+
+let powerUp = FireFlower
+match powerUp with
+| FireFlower -> printfn "Ouch, that's hot!"
+| Mushroom -> printfn "Please don't step on me..."
+| Star -> printfn "Let me play some special music for you."
+# Output
+Ouch, that's hot!
+
+val powerUp : PowerUp = FireFlower
+val it : unit = ()
+
+let handlePowerUp powerUp =
+    match powerUp with
+    | FireFlower -> printfn "Ouch, that's hot!"
+    | Mushroom color -> match color with
+                        | Red -> printfn "Please don't step on me..."
+                        | Green -> printfn "1UP!!!"
+                        | Purple -> printfn "Sorry, about that!"
+    | Star duration -> printfn "Let me play some special music for you 
+        for %d seconds." duration
+
+// Test handlePowerUp.
+let powerUp = Star 14
+handlePowerUp powerUp
